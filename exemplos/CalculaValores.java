404bea75ab17b3c212d2639031f32b61e43a9c28
alicio.neto <alicio.neto@senior.com.br>
2020-07-30 20:50:27 -0300

    declarado as variaveis

--- exemplos/CalculaValores.java
+++ exemplos/CalculaValores.java
@@ -1,9 +1,18 @@
 public class CalculaValores {
     
 	public static void main (String[] args) {
+
+
+        int resultado;
+
+        int nummeroUm;
+        int nummeroDois;    
+        int soma;
+
+        // irá fazer um calculo de soma
         
 
-		System.out.println("");
+		System.out.println("O resultado da soma: " + resultado);
 	}
 }
 
