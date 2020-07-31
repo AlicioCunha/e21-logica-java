f0e55b9bfbe95d667788eca4dbd73e2fa71f98d6
alicio.neto <alicio.neto@senior.com.br>
2020-07-30 21:09:26 -0300

    multiplicando

--- exemplos/CalculaValores.java
+++ exemplos/CalculaValores.java
@@ -2,9 +2,9 @@ public class CalculaValores {
     
 	public static void main (String[] args) {
 
-        int nummeroUm;
-        int nummeroDois;    
-        int soma, substracao;
+        double nummeroUm;
+        double nummeroDois;    
+        double soma, substracao, divisao, multiplicacao;
 
         // irá fazer um calculo de soma
         nummeroUm = 10 ;
@@ -12,9 +12,13 @@ public class CalculaValores {
 
         soma = nummeroUm + nummeroDois;
         substracao = nummeroUm - nummeroDois;
+        divisao = nummeroUm / nummeroDois;
+        multiplicacao = nummeroUm * nummeroDois;
 
         System.out.println("O resultado da soma: " + soma);
-        System.out.println("O resultado da soma: " + substracao);
+        System.out.println("O resultado da subtração: " + substracao);
+        System.out.println("O resultado da divisão: " + divisao);
+        System.out.println("O resultado da multiplicação: " + multiplicacao);
 	}
 }
 
